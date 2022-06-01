2c4e0cd838aec423218bb81cd1b58a99f872e2d3
Yara Valentina Muñoz Tangarife <yaravalentina@iMac-de-Yara.local>
2022-06-01 15:12:13 -0500

    make it better

--- ExplosionConstruccion/index.php
+++ ExplosionConstruccion/index.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Explosión Culinaria</title>
-</head>
-
-<body>
-    <img src="LGO BLANCO.png" alt="">
-    <h1>Pagina en Construcción...</h1>
-    <h2>Proximamente el mejor festival gastronómico</h2>
-</body>
-
-</html>
+<?php include_once("index.htlm"); ?>
